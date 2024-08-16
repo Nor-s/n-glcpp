546f016483e1bc9207c487d53e362ffed3e7fd65
nor-s <nors.nsg@gmail.com>
2024-08-16 23:49:13 +0900

    python: fix error "pip install mediapipe"

--- scripts/build_python.bat
+++ scripts/build_python.bat
@@ -16,12 +16,12 @@ if exist %mypath%\..\python\python.exe (
      echo -- END::Python build 
 
      echo -- START::LAYOUT    
-     
            %cpython_path%\PCBuild\amd64\python.exe %cpython_path%PC\layout\main.py -b %cpython_path%\PCBuild\amd64  -s %cpython_path% -t %cpython_path%\temp\  --include-underpth --include-stable --include-pip --include-dev --include-launchers  --include-venv --include-symbols --copy %install_path%
+          %cpython_path%\PCBuild\amd64\python.exe -m pip install --upgrade pip
 
      echo -- END::LAYOUT 
      echo -- START::PIP INSTALL 
-          %install_path%\python.exe -m pip install mediapipe==0.8.10.1
+          %install_path%\python.exe -m pip install mediapipe
           %install_path%\python.exe -m pip  install PyGLM==2.5.7
      echo -- END::PIP INSTALL 
 )
