8438fb325296fa37e80aae47429c3be7b8627db6
nor-s <nors.nsg@gmail.com>
2024-08-21 00:33:13 +0900

    animation: Separate json import logic for transform

--- src/animation/json_animation.h
+++ src/animation/json_animation.h
@@ -1,50 +1,50 @@
 /**
   // json 형태
 {
-    "fileName": "sample/mixamo_dance.gif",
-    "duration": 1,
-    "ticksPerSecond": 1,
-    "bindpose": {
-      "bone_name1": [
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1
-      ],
-      "bone_name2": [
-        1, 0, 0, 0,
-        0, 1, 0, 0,
-        0, 0, 1, 0,
-        0, 0, 0, 1
-      ]
-      ...
-    },
-    "frames": [
-        {
-            "time": 0,
-            "bones": [
-                {
-                    "name": "neck",
-                    "rotation": {
-                        "w": 1.0,
-                        "x": 0.1,
-                        "y": 0.2,
-                        "z": 0.3
-                    },
-                    "position": {
-                        "x": 0.0,
-                        "y": 0.0,
-                        "z": 0.0
-                    },
-                    "scale": {
-                        "x": 1.0,
-                        "y": 1.0,
-                        "z": 1.0
-                    }
-                }
-            ]
-        }
-    ]
+	"fileName": "sample/mixamo_dance.gif",
+	"duration": 1,
+	"ticksPerSecond": 1,
+	"bindpose": {
+	  "bone_name1": [
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	  ],
+	  "bone_name2": [
+		1, 0, 0, 0,
+		0, 1, 0, 0,
+		0, 0, 1, 0,
+		0, 0, 0, 1
+	  ]
+	  ...
+	},
+	"frames": [
+		{
+			"time": 0,
+			"bones": [
+				{
+					"name": "neck",
+					"rotation": {
+						"w": 1.0,
+						"x": 0.1,
+						"y": 0.2,
+						"z": 0.3
+					},
+					"position": {
+						"x": 0.0,
+						"y": 0.0,
+						"z": 0.0
+					},
+					"scale": {
+						"x": 1.0,
+						"y": 1.0,
+						"z": 1.0
+					}
+				}
+			]
+		}
+	]
 }
  */
 #ifndef ANIM_ANIMATION_JSON_ANIMATION_H
@@ -57,33 +57,35 @@
 
 namespace anim
 {
-  class JsonAnimation : public Animation
-  {
-  public:
-    JsonAnimation() = delete;
-    JsonAnimation(const char *animation_path);
+	class JsonUtil
+	{
+	public:
+		static glm::vec3 GetPosition(const Json::Value& bone);
 
-    ~JsonAnimation();
+		static glm::quat GetRotation(const Json::Value& bone);
 
-    void reload() override;
+		static glm::vec3 GetScale(const Json::Value& bone);
+		static glm::mat4 GetMat(const Json::Value& mat_array);
+	};
+	class JsonAnimation : public Animation
+	{
+	public:
+		JsonAnimation() = delete;
+		JsonAnimation(const char* animation_path);
 
-  private:
-    void destroy();
+		~JsonAnimation();
 
-    void init();
+		void reload() override;
 
-    void process_frames(const Json::Value &frames);
+	private:
+		void destroy();
 
-    void process_bones(const Json::Value &bones, float time);
+		void init();
 
-    void process_bindpose(const Json::Value &bindpose);
+		void process_frames(const Json::Value& frames);
+		void process_bones(const Json::Value& bones, float time);
+		void process_bindpose(const Json::Value& bindpose);
 
-    glm::vec3 get_position(const Json::Value &bone);
-
-    glm::quat get_rotation(const Json::Value &bone);
-
-    glm::vec3 get_scale(const Json::Value &bone);
-    glm::mat4 get_mat(const Json::Value &mat_array);
-  };
+	};
 }
 #endif
